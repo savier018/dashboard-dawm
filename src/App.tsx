d0f0514638cbb48b2994cf237ac2d1b96c12e989
savier018 <acosta.savier.03@gmail.com>
2024-07-05 15:21:45 -0500

    editando...

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Grid>
-        <Grid>1</Grid>
-        <Grid>2</Grid>
-        <Grid>3</Grid>
-        <Grid>4</Grid>
-        <Grid>5</Grid>
-        <Grid>6</Grid>
-      </Grid>
+    <Grid container spacing={5}>
+    <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
+    <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
+    <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
+    <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
+    <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
+    <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+  </Grid>
   )
 }
 
