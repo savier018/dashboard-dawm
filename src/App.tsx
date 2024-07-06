65324c2fd380a3983688e7ddf4f2059c4cea7af0
savier018 <acosta.savier.03@gmail.com>
2024-07-05 20:33:24 -0500

    funcion async para la tabla

--- src/App.tsx
+++ src/App.tsx
@@ -39,17 +39,44 @@ function App() {
         en el arreglo de resultados
                    */}
 
-      let location = xml.getElementsByTagName("location")[1]
+      let nombreCiudad = xml.getElementsByTagName("name")[0]
+      const ciudad = nombreCiudad.textContent;
 
-      let geobaseid = location.getAttribute("geobaseid")
-      dataToIndicators.push(["Location", "geobaseid", geobaseid])
+      let nombrePais = xml.getElementsByTagName("country")[0]
+      const pais = nombrePais.textContent;
+
+      let location = xml.getElementsByTagName("location")[1]
+      dataToIndicators.push([ciudad, "País", pais])
 
       let latitude = location.getAttribute("latitude")
-      dataToIndicators.push(["Location", "Latitude", latitude])
+      dataToIndicators.push(["Latitud", "Valor", latitude])
 
       let longitude = location.getAttribute("longitude")
-      dataToIndicators.push(["Location", "Longitude", longitude])
+      dataToIndicators.push(["Longitud", "Valor", longitude])
 
+      let valorPrecipitacion = xml.getElementsByTagName("precipitation")[0]
+      let probabilidadPrecipitacion = valorPrecipitacion.getAttribute("probability")
+      dataToIndicators.push(["Precipitación", "Probabilidad", probabilidadPrecipitacion])
+
+      let velocidadViento = xml.getElementsByTagName("windSpeed")[0]
+      let valorVelocidadViento = velocidadViento.getAttribute("mps")
+      let unidadesVelocidadViento = velocidadViento.getAttribute("unit")
+      dataToIndicators.push(["Velocidad del viento", unidadesVelocidadViento, valorVelocidadViento])
+
+      let temperatura = xml.getElementsByTagName("temperature")[0]
+      let valorTemperatura = temperatura.getAttribute("value")
+      let valorCelsius = parseFloat(valorTemperatura) - 273.15
+      let valorCelsiusFixed = parseFloat(valorCelsius.toFixed(2));
+      let valorCelsiusFixedConGrado = `${valorCelsiusFixed}°C`;
+      dataToIndicators.push(["Temperatura", "Celsius", valorCelsiusFixedConGrado])
+
+
+      let seSienteComo = xml.getElementsByTagName("feels_like")[0]
+      let valorSeSienteComo = seSienteComo.getAttribute("value")
+      let valorCelsiusSeSienteComo = parseFloat(valorSeSienteComo) - 273.15
+      let valorCelsiusSeSienteComoFixed = parseFloat(valorCelsiusSeSienteComo.toFixed(2))
+      let valorCelsiusSeSienteComoConGrado = `${valorCelsiusSeSienteComoFixed}°C`;
+      dataToIndicators.push(["Se siente como", "Celsius", valorCelsiusSeSienteComoConGrado]);
       //console.log(dataToIndicators)
 
       {/* Renderice el arreglo de resultados en un arreglo de elementos Indicator */ }
@@ -68,7 +95,7 @@ function App() {
 
   return (
     <Grid container spacing={5}>
-      <Grid xs={6} lg={2}>
+      <Grid xs={6} lg={4}>
 
         {indicators[0]}
 
@@ -76,7 +103,7 @@ function App() {
 
       </Grid>
 
-      <Grid xs={6} lg={2}>
+      <Grid xs={6} lg={4}>
 
         {indicators[1]}
 
@@ -84,27 +111,60 @@ function App() {
 
       </Grid>
 
-      <Grid xs={6} lg={2}>
+      <Grid xs={6} lg={4}>
 
         {indicators[2]}
 
         {/* <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /> */}
 
       </Grid>
-      <Grid xs={6} md={4} lg={6}>
-        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+
+      <Grid xs={6} md={4} lg={3}>
+
+        {indicators[3]}
+
       </Grid>
-      <Grid xs={6} sm={4} md={3} lg={6}>
+
+      <Grid xs={6} md={4} lg={3}>
+
+        {indicators[4]}
+
+      </Grid>
+
+      <Grid xs={6} md={4} lg={3}>
+
+        {indicators[5]}
+
+      </Grid>
+
+      <Grid xs={6} md={4} lg={3}>
+
+        {indicators[6]}
+
+      </Grid>
+
+      {/* <Grid xs={6} sm={4} md={3} lg={6}>
+
         <Summary></Summary>
-      </Grid>
+
+      </Grid> */}
+
       <Grid xs={12} md={6} lg={12} >
+
         <BasicTable />
+
       </Grid>
-      <Grid xs={12} lg={10}>
+
+      <Grid xs={12} lg={8}>
+
         <WeatherChart></WeatherChart>
+
       </Grid>
-      <Grid xs={12} lg={2}>
+
+      <Grid xs={12} lg={4}>
+
         <ControlPanel />
+
       </Grid>
     </Grid>
   )
--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,54 +7,85 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function BasicTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+
+    const [weatherData, setWeatherData] = useState([]);
+
+    {/* Hook: useEffect */ }
+
+    useEffect(() => {
+
+        (async () => {
+
+            {/* Request */ }
+
+            let API_KEY = "acd89c554aa31e18546c9194118b8bea"
+            let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+            let savedTextXML = await response.text();
+
+            {/* XML Parser */ }
+
+            const parser = new DOMParser();
+            const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+            {/* Arreglo para agregar los resultados */ }
+
+            let dataTable = new Array()
+            let tiempo = xml.getElementsByTagName('time');
+
+            for (let i = 0; i < tiempo.length; i++) {
+                const tiempoDesde = tiempo[i].getAttribute("from")
+                const tiempoHasta = tiempo[i].getAttribute("to")
+
+                let temperatura = tiempo[i].getElementsByTagName("temperature")[0]
+                let valorTemperatura = temperatura.getAttribute("value")
+                let valorCelsius = parseFloat(valorTemperatura) - 273.15
+                let valorCelsiusFixed = parseFloat(valorCelsius.toFixed(2));
+                let valorCelsiusFixedConGrado = `${valorCelsiusFixed}°C`;
+
+                let velocidadViento = tiempo[i].getElementsByTagName("windSpeed")[0]
+                let valorVelocidadViento = velocidadViento.getAttribute("mps")
+
+                let humedad = tiempo[i].getElementsByTagName("humidity")[0]
+                let valorHumedad = humedad.getAttribute("value")
+
+                let presion = tiempo[i].getElementsByTagName("pressure")[0]
+                let valorPresion = presion.getAttribute("value")
+
+
+                dataTable.push({ tiempoDesde, tiempoHasta, valorCelsiusFixedConGrado, valorVelocidadViento, valorHumedad, valorPresion })
+            }
+
+            setWeatherData(dataTable);
+        })()
+    }, [])
+
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Hora (desde)</TableCell>
+                        <TableCell>Hora (hasta)</TableCell>
+                        <TableCell align="center">Temperatura (°C)</TableCell>
+                        <TableCell align="center">Velocidad del Viento (m/s)</TableCell>
+                        <TableCell align="center">Humedad (%)</TableCell>
+                        <TableCell align="center">Presión (hPa)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {weatherData.map((row, index) => (
+                        <TableRow key={index}>
+                            <TableCell component="th" scope="row">{row.tiempoDesde}</TableCell>
+                            <TableCell component="th" scope="row">{row.tiempoHasta}</TableCell>
+                            <TableCell align="center">{row.valorCelsiusFixedConGrado}</TableCell>
+                            <TableCell align="center">{row.valorVelocidadViento}</TableCell>
+                            <TableCell align="center">{row.valorHumedad}</TableCell>
+                            <TableCell align="center">{row.valorPresion}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
